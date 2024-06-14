17195056d1439e9626c3852de9ccb5ab05ce6363
JREstrada <joelestrada021604@gmail.com>
2024-06-14 21:21:11 +0800

    (Create Account) STAFF ACCOUNT ACTIVATION file?
    Hindi ko alam gagawin dito sa GitHub.

---
+++ HotelManagementSystem/HotelManagementSystem/StaffAccountActivation/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace StaffAccountActivation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            Passwrod_TextBox = new TextBox();
+            textBox8 = new TextBox();
+            label9 = new Label();
+            PasswordShow_CheckBox = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Garamond", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Transparent;
+            label1.Location = new Point(660, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(580, 68);
+            label1.TabIndex = 2;
+            label1.Text = "Activate your account";
+            label1.Click += label1_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.None;
+            textBox1.BackColor = Color.Gainsboro;
+            textBox1.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(581, 178);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(730, 27);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Transparent;
+            label2.Location = new Point(581, 156);
+            label2.Name = "label2";
+            label2.Size = new Size(154, 19);
+            label2.TabIndex = 4;
+            label2.Text = "Employee Number*";
+            label2.Click += label2_Click;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = Color.DarkGoldenrod;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Garamond", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.WhiteSmoke;
+            button1.Location = new Point(581, 610);
+            button1.Name = "button1";
+            button1.Size = new Size(730, 43);
+            button1.TabIndex = 5;
+            button1.Text = "Confirm";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(-4, -4);
+            pictureBox2.Margin = new Padding(0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(449, 767);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Transparent;
+            label3.Location = new Point(581, 218);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 19);
+            label3.TabIndex = 7;
+            label3.Text = "First Name*";
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.None;
+            textBox2.BackColor = Color.Gainsboro;
+            textBox2.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(581, 240);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(582, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Transparent;
+            label4.Location = new Point(581, 279);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 19);
+            label4.TabIndex = 9;
+            label4.Text = "Middle Name";
+            label4.Click += label4_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.None;
+            textBox3.BackColor = Color.Gainsboro;
+            textBox3.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(581, 301);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(730, 27);
+            textBox3.TabIndex = 8;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.None;
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Transparent;
+            label5.Location = new Point(581, 340);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 19);
+            label5.TabIndex = 11;
+            label5.Text = "Last Name*";
+            label5.Click += label5_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.None;
+            textBox4.BackColor = Color.Gainsboro;
+            textBox4.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(581, 362);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(730, 27);
+            textBox4.TabIndex = 10;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.None;
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Transparent;
+            label6.Location = new Point(581, 401);
+            label6.Name = "label6";
+            label6.Size = new Size(127, 19);
+            label6.TabIndex = 13;
+            label6.Text = "Phone Number*";
+            label6.Click += label6_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.None;
+            textBox5.BackColor = Color.Gainsboro;
+            textBox5.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(581, 423);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(730, 27);
+            textBox5.TabIndex = 12;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.None;
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Transparent;
+            label7.Location = new Point(581, 463);
+            label7.Name = "label7";
+            label7.Size = new Size(121, 19);
+            label7.TabIndex = 15;
+            label7.Text = "Email Address*";
+            label7.Click += label7_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Anchor = AnchorStyles.None;
+            textBox6.BackColor = Color.Gainsboro;
+            textBox6.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox6.Location = new Point(581, 485);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(730, 27);
+            textBox6.TabIndex = 14;
+            textBox6.TextChanged += textBox6_TextChanged;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.None;
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Transparent;
+            label8.Location = new Point(581, 525);
+            label8.Name = "label8";
+            label8.Size = new Size(85, 19);
+            label8.TabIndex = 17;
+            label8.Text = "Password*";
+            label8.Click += label8_Click;
+            // 
+            // Passwrod_TextBox
+            // 
+            Passwrod_TextBox.Anchor = AnchorStyles.None;
+            Passwrod_TextBox.BackColor = Color.Gainsboro;
+            Passwrod_TextBox.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Passwrod_TextBox.Location = new Point(581, 547);
+            Passwrod_TextBox.Name = "Passwrod_TextBox";
+            Passwrod_TextBox.Size = new Size(582, 27);
+            Passwrod_TextBox.TabIndex = 16;
+            Passwrod_TextBox.UseSystemPasswordChar = true;
+            Passwrod_TextBox.TextChanged += Passwrod_TextBox_TextChanged;
+            // 
+            // textBox8
+            // 
+            textBox8.Anchor = AnchorStyles.None;
+            textBox8.BackColor = Color.Gainsboro;
+            textBox8.Font = new Font("Garamond", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox8.Location = new Point(1169, 240);
+            textBox8.Name = "textBox8";
+            textBox8.PlaceholderText = "i.e. Sr., Jr., III, VI";
+            textBox8.Size = new Size(142, 27);
+            textBox8.TabIndex = 18;
+            textBox8.TextChanged += textBox8_TextChanged;
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.None;
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Garamond", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Transparent;
+            label9.Location = new Point(1169, 218);
+            label9.Name = "label9";
+            label9.Size = new Size(51, 19);
+            label9.TabIndex = 19;
+            label9.Text = "Suffix";
+            label9.Click += label9_Click;
+            // 
+            // PasswordShow_CheckBox
+            // 
+            PasswordShow_CheckBox.AutoSize = true;
+            PasswordShow_CheckBox.BackColor = Color.Transparent;
+            PasswordShow_CheckBox.Font = new Font("Garamond", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PasswordShow_CheckBox.ForeColor = Color.Transparent;
+            PasswordShow_CheckBox.Location = new Point(1198, 548);
+            PasswordShow_CheckBox.Name = "PasswordShow_CheckBox";
+            PasswordShow_CheckBox.Size = new Size(75, 26);
+            PasswordShow_CheckBox.TabIndex = 20;
+            PasswordShow_CheckBox.Text = "Show";
+            PasswordShow_CheckBox.UseVisualStyleBackColor = false;
+            PasswordShow_CheckBox.CheckedChanged += PasswordShow_CheckBox_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(23F, 45F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MidnightBlue;
+            ClientSize = new Size(1370, 759);
+            Controls.Add(PasswordShow_CheckBox);
+            Controls.Add(label9);
+            Controls.Add(textBox8);
+            Controls.Add(label8);
+            Controls.Add(Passwrod_TextBox);
+            Controls.Add(label7);
+            Controls.Add(textBox6);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Font = new Font("Garamond", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(9, 7, 9, 7);
+            Name = "Form1";
+            Text = "Staff Account Activation";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label6;
+        private TextBox textBox5;
+        private Label label7;
+        private TextBox textBox6;
+        private Label label8;
+        private TextBox Passwrod_TextBox;
+        private TextBox textBox8;
+        private Label label9;
+        private CheckBox PasswordShow_CheckBox;
+    }
+}
